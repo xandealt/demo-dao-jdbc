5c3835c03a162243bd0f5ebb80f7a7ec3df31286
Alexandre Augusto <alexandre_alt@outlook.com>
2023-06-12 16:59:24 -0300

    Department entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,11 @@
 package application;
 
+import model.entities.Department;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Department obj = new Department(1, "Test");
+
+        System.out.println(obj);
     }
 }
