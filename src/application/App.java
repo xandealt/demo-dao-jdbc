e262e48df44878a307f235fcfb2ca729fb01f1d1
Alexandre Augusto <alexandre_alt@outlook.com>
2023-06-12 17:11:33 -0300

    Seller entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "Test");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "bob", "bob@gamil", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
